b4b4b9fb3abaf2fb5189acf5a0118837e8d77100
amirhosseinaarabi2002 <amiraarabisw2002@gmail.com>
2024-10-29 15:43:59 +0330

    start

---
+++ pages/homes/[id].js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SingleHomeDetailsPage = () => {
+  return (
+    <div>Single Home Details Page</div>
+  )
+}
+
+export default SingleHomeDetailsPage
---
+++ pages/homes/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>all homes</div>
+  )
+}
+
+export default index
